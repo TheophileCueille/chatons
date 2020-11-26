77e83750f8cd2304aa2dd183e80a4bf0aeb8cd93
Théophile <theophile.cueille@gmail.com>
2020-11-26 23:43:58 +0100

    heroku+sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,5 +65,9 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   #------aide stackoverflow-----
-  
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
 end
