fab9acb56998f468a10d2eb0f132d05e0710662f
Théophile <theophile.cueille@gmail.com>
2020-11-27 01:29:50 +0100

    heroku+sendgrid

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,14 @@ class UserMailer < ApplicationMailer
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
+
+  def confirmation_instructions(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Confirme bordel !') 
+  end
 end
