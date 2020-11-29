aacf0b4ea64f49586db67d30c70db464d6916884
Théophile <theophile.cueille@gmail.com>
2020-11-29 22:53:59 +0100

    debug carts creation

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,6 +1,6 @@
 module CurrentCart
     def set_cart
-        @cart = Cart.find_or_create_by( user_id: 1 )#user_id: current_user.id)
+        @cart = Cart.find_or_create_by(user_id: current_user.id)
     end
     
 end
